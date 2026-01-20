67375c8cda7ff85819ee49c5cf08c36d78cac154
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-20 19:57:55 +0000

    yippie doo dah

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+let lengthInFeet = 1.5;
+let pricePerFoot = 2.50;
+let totalPrice = lengthInFeet * pricePerFoot;
+document.getElementById("result").innerHTML = totalPrice;
 
+
+
+
+
+//should assign a variable lengthInFeet to 1.5
+//should assign a variable pricePerFoot to 2.50
+//should capture the product of lengthInFeet and pricePerFoot multiplied together in a variable named totalPrice
+//should assign the innerHTML of the HTML element with the id result to the totalPrice
